342fb5515cfbdbfcc7a8a526d747edc1eb3acc1f
minichen <minichen@qq.com>
2016-02-25 17:01:54 +0800

    ..

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -408,9 +408,6 @@ function MiddleNEController($scope, statasticService, logger, $state, commonUtil
     
 
     commonUtil.genAgGridWatchDelayReloader($scope, vm.dataChangeTrigger, vm.gridOptions, 0, dontApplyFun);       
-            
-   
-    
 }
 
 angular
@@ -439,6 +436,151 @@ angular
 
 MiddleTrailController.$inject = ['$stateParams','retrievedSNCs', 'logger', '$state', '$scope','commonUtil', 'serverNotificationService', '$timeout'];
 function MiddleTrailController($stateParams, retrievedSNCs, logger, $state, $scope, commonUtil, serverNotificationService, $timeout) {
+    var vm = this;
+    vm.getH=commonUtil.getH;
+    vm.ctrlScope=$scope;
+    vm.dataArray=retrievedSNCs;
+    vm.dataKey='sncKey';
+    vm.objCreationNtType='sncCreation';
+    vm.objDeletionNtType='sncDeletion';
+    vm.objUpdateNtType='sncUpdate';
+    vm.notifFilterFun=null;
+    vm.ctrlName='MiddleTrailController';   
+    
+    
+    logger.log("vm.dataKey:"+vm.dataKey);
+    
+    vm.fieldValueGetterFun=function(item, field){
+        if(field==='aEndNE'){
+            return item.aEndPorts[0].neName;
+        }else if(field==='aEndTP'){
+            return item.aEndPorts[0].tpName;
+        }else if(field==='zEndNE'){
+            return item.zEndPorts[0].neName;
+        }else if(field==='zEndTP'){
+            return item.zEndPorts[0].tpName;
+        }else{
+            return item[field];
+        }
+    }
+     
+    vm.tableColumnDefs=[
+        {
+            field: "name",
+            headerName: "名称",
+            width: commonUtil.getW(120),
+            minWidth: commonUtil.getW(120),
+            filter: 'text',
+            filterParams: {newRowsAction: 'keep'},
+            pinned: 'left',
+            onCellClicked: function(params){
+                    commonUtil.treeNavWithLoadingPage($state, $timeout, 'main.treeitem_secondlevel', {treeItemId: 'trail', sncId: params.data.sncId});
+                },
+            cellClass: 'table-name-field'
+        },
+         {
+            field: "sncId",
+            headerName: "子网连接ID",
+            width: commonUtil.getW(120),
+            minWidth: commonUtil.getW(120),
+            filter: 'text',
+            filterParams: {newRowsAction: 'keep'}
+        },
+        {
+            field: "rate",
+            headerName: "层次",
+            width: commonUtil.getW(90),
+            minWidth: commonUtil.getW(90),
+            filter: 'text',
+            filterParams: {newRowsAction: 'keep'}
+        },
+        {
+            field: "protectedType",
+            headerName: "保护",
+            width: commonUtil.getW(110),
+            minWidth: commonUtil.getW(110),
+            filter: 'set',
+            filterParams: {values: ['protected', 'unprotected'], newRowsAction: 'keep'},
+            cellRenderer: function(params){
+                var cls='table-snc-protect-'+params.value;
+                return "<div class='"+cls+"'>"+params.value+"</div>";
+            }
+        },
+        {
+            field: "sncState",
+            headerName: "实施状态",
+            width: commonUtil.getW(110),
+            minWidth: commonUtil.getW(110),
+            filter: 'set',
+            filterParams: {values: ['defined', 'allocated','implemented'], newRowsAction: 'keep'},
+            cellRenderer: function(params){
+                var cls='table-snc-state-'+params.value;
+                return "<div class='"+cls+"'>"+params.value+"</div>";
+            }
+        }, 
+        {
+            field: "aEndNE",
+            headerName: "A端网元",
+            valueGetter: function(params){
+                return params.data.aEndPorts[0].neName;
+            },
+            onCellClicked: function(params){
+                    $state.go('main.treeitem_secondlevel',{treeItemId: 'ne', neGroupId: params.data.aEndPorts[0].neGroupId, neId: params.data.aEndPorts[0].neId });
+                },
+            cellClass: 'table-name-field',
+            width: commonUtil.getW(120),
+            minWidth: commonUtil.getW(120),
+            filter: 'text',
+            filterParams: {newRowsAction: 'keep'}
+        },
+        {
+            field: "aEndTP",
+            headerName: "A端TP",
+            valueGetter: function(params){
+                return params.data.aEndPorts[0].tpName;
+            },
+            width: commonUtil.getW(120),
+            minWidth: commonUtil.getW(120),
+            filter: 'text',
+            filterParams: {newRowsAction: 'keep'}
+        },
+        {
+            field: "zEndNE",
+            headerName: "Z端网元",
+            valueGetter: function(params){
+                return params.data.zEndPorts[0].neName;
+            },
+            onCellClicked: function(params){
+                    $state.go('main.treeitem_secondlevel',{treeItemId: 'ne', neGroupId: params.data.zEndPorts[0].neGroupId, neId: params.data.zEndPorts[0].neId });
+                },
+            cellClass: 'table-name-field',
+            width: commonUtil.getW(120),
+            minWidth: commonUtil.getW(120),
+            filter: 'text',
+            filterParams: {newRowsAction: 'keep'}
+        },
+        {
+            field: "zEndTP",
+            headerName: "Z端TP",
+            valueGetter: function(params){
+                return params.data.zEndPorts[0].tpName;
+            },
+            width: commonUtil.getW(120),
+            minWidth: commonUtil.getW(120),
+            filter: 'text',
+            filterParams: {newRowsAction: 'keep'}
+        }
+    ];
+    
+    
+}
+
+angular
+    .module('nmsdemoApp')
+    .controller('MiddleTrailController2', MiddleTrailController2);
+
+MiddleTrailController2.$inject = ['$stateParams','retrievedSNCs', 'logger', '$state', '$scope','commonUtil', 'serverNotificationService', '$timeout'];
+function MiddleTrailController2($stateParams, retrievedSNCs, logger, $state, $scope, commonUtil, serverNotificationService, $timeout) {
     var vm = this;
     vm.getH=commonUtil.getH;
     vm.tableSizeClassFun=function(dim){
--- src/main/webapp/js/directives.js
+++ src/main/webapp/js/directives.js
@@ -51,7 +51,10 @@ angular
                     }
                 }
             };
-        })
+        });
+        
+angular
+    .module('nmsdemoApp')
     .directive('maintree',
         function () {
             return {
@@ -73,3 +76,83 @@ angular
                 replace: false
             };
         });
+
+angular
+    .module('nmsdemoApp')
+    .directive('nmsDataPanel', NmsDataPanel);
+NmsDataPanel.$inject = ['logger', 'commonUtil', 'serverNotificationService'];
+function NmsDataPanel(logger, commonUtil, serverNotificationService) {
+    return {
+        // can be used as attribute or element
+        restrict: 'AE',
+        scope: {
+            ctrlScope: '=',
+            dataArray: '=',
+            dataKey: '=',
+            tableColumnDefs: '=',
+            objCreationNtType: '=',
+            objDeletionNtType: '=',
+            objUpdateNtType: '=',
+            notifFilterFun: '=',
+            ctrlName: '=',
+            gridHeight: '=',
+            fieldValueGetterFun: '='
+        },
+        // which markup this directive generates
+        templateUrl: './partials/nms-data-panel.html?dummy',
+        replace: true,
+        link: function (scope, iElement, iAttrs) {
+            //logger.log("scope:"+JSON.stringify(scope));
+            
+            logger.log("scope.dataKey: "+scope.dataKey);
+            scope.tableSizeClass = { width: '100%', height: "" + commonUtil.getH(scope.gridHeight) + "px" };
+            var objSearchMap = new commonUtil.ObjectArrayKeyIndexManager(scope.dataArray, scope.dataKey);
+            scope.data = objSearchMap.getArray();
+            scope.dataChangeTrigger = new commonUtil.WatchTrigger();
+            scope.enableNotif = true;
+
+            function addObj(obj) {
+                if (objSearchMap.add(obj)) {
+                    scope.dataChangeTrigger.trigger();
+                }
+            }
+            function removeObj(obj) {
+                if (objSearchMap.remove(obj[scope.sncKey])) {
+                    scope.dataChangeTrigger.trigger();
+                }
+            }
+            function updateObj(obj) {
+                objSearchMap.add(obj);
+                scope.dataChangeTrigger.trigger();
+            }
+            function eventListener(event) {
+                // logger.log(scope.ctrlName+" event:"+JSON.stringify(event));
+                if (event.eventType == scope.objCreationNtType) {
+                    addObj(event.event);
+                    // logger.log(scope.ctrlName+": objCreation:\n" + JSON.stringify(event.event));
+                } else if (event.eventType == scope.objDeletionNtType) {
+                    removeObj(event.event);
+                    // logger.log(scope.ctrlName+": objDeletion:\n" + JSON.stringify(event.event));
+                } else if (event.eventType == scope.objUpdateNtType) {
+                    updateObj(event.event);
+                    // logger.log(scope.ctrlName+": objUpdate:\n" + JSON.stringify(event.event));
+                }
+            }
+
+            function dontApplyFun() {
+                return !scope.enableNotif;
+            }
+            scope.gridOptions = commonUtil.genAgGridOptions(scope.tableColumnDefs, scope.data, scope.fieldValueGetterFun, null);
+            var listener = commonUtil.genDelayScopeApplyEventListener(scope.ctrlScope, scope.notifFilterFun, [scope.objCreationNtType, scope.objDeletionNtType, scope.objUpdateNtType], eventListener, 200, scope.ctrlName, dontApplyFun);
+            serverNotificationService.addListener(listener);
+
+            scope.ctrlScope.$on("$destroy", function () {
+                logger.log(scope.ctrlName + ",$destroy");
+                serverNotificationService.removeListener(listener);
+            });
+
+            commonUtil.genAgGridWatchDelayReloader(scope.ctrlScope, scope.dataChangeTrigger, scope.gridOptions, 0, dontApplyFun);
+        }
+    };
+}
+
